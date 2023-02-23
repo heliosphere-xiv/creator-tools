23e7259d4dc8dc71c65d167a6015074101bb0348
Anna Clemens <git@annaclemens.io>
2023-02-22 19:45:32 -0500

    fix: use tauri fetch again

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -1,27 +1,39 @@
 import { HoudiniClient } from '$houdini';
 import { get } from 'svelte/store';
 import { authToken } from '$lib/stores';
+import { Body, fetch } from '@tauri-apps/api/http';
+import { fetch as houdiniFetchPlugin } from '$houdini/plugins';
+import type { RequestHandlerArgs } from 'houdini/build/runtime/client/plugins';
 
-function authHeader(): { 'x-auth-token': string } | {} {
+async function fetchQuery(args: RequestHandlerArgs) {
+    const {
+        text = '',
+        variables = {},
+    } = args;
+
+    const headers: Record<string, string> = {
+        'Content-Type': 'application/json',
+        'User-Agent': 'hs-creator-tools/1.2.0',
+    };
+
     const auth = get(authToken);
     if (auth) {
-        return {
-            'x-auth-token': auth,
-        };
+        headers['x-auth-token'] = auth;
     }
 
-    return {};
+    const resp = await fetch('https://heliosphere.app/api/api/graphql', {
+        method: 'POST',
+        headers,
+        body: Body.json({
+            query: text,
+            variables,
+        }),
+    });
+
+    return resp.data as any;
 }
 
 export default new HoudiniClient({
     url: 'https://heliosphere.app/api/api/graphql',
-    fetchParams({ session }) {
-        return {
-            headers: {
-                'Content-Type': 'application/json',
-                'User-Agent': 'hs-creator-tools/1.2.0',
-                ...authHeader(),
-            },
-        };
-    },
+    plugins: [houdiniFetchPlugin(fetchQuery)],
 });
