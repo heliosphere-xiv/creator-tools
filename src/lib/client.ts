f6197a0b994f27e66b9751bf6417026435d5691c
Anna <git@anna.lgbt>
2024-07-25 21:57:45 -0400

    chore: bump version to 2.0.1

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -14,7 +14,7 @@ async function fetchQuery(args: RequestHandlerArgs) {
 
     const headers: Record<string, string> = {
         'Content-Type': 'application/json',
-        'User-Agent': 'hs-creator-tools/2.0.0',
+        'User-Agent': 'hs-creator-tools/2.0.1',
     };
 
     const resp = await fetch(URL, {
